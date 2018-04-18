8d350beb25d2ce9e25ab1d136f0756d5414ac52c
Amimaro <amir.zahlan@gmail.com>
2018-04-18 19:26:23 -0300

    Create getModulePath method :sparkles:

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 'use strict'
 
-const ejs = require('ejs')
 const fs = require('fs')
+const ejs = require('ejs')
+const path = require('path')
+const stream = require('stream')
+const pdfjsLib = require('pdfjs-dist')
 
 module.exports = async function pdfTemplate(params) {
+  require(getModulePath() + '/vendor/pdfjs/domstubs.js').setStubs(global)
   return params
+}
+
+let getModulePath = function() {
+  return process.cwd()+'/node_modules/pdf-template';
 }
