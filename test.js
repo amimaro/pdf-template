08433d69ae641553a4a0a8571c327c622f9392d3
Amimaro <amir.zahlan@gmail.com>
2018-04-19 10:28:25 -0300

    Create tests :rotating_light:

--- test.js
+++ test.js
@@ -1,8 +1,36 @@
 import test from 'ava'
 import pdfTemplate from './'
 
-test('pdf-template does something awesome', t => {
+test('pdf-template example', t => {
   t.plan(1)
+  return pdfTemplate({
+    template: './example.pdf',
+    output: './output.pdf',
+    data: {
+      name: 'John Doe',
+      age: 26,
+      email: 'johndoe@example.com',
+      birthdate: '01/01/1990',
+      projects: ['project1', 'project2', 'project3']
+    }
+  }).then((res) => {
+    t.true(res)
+  })
+})
 
-  t.true(pdfTemplate())
+test('pdf-template fail example', t => {
+  t.plan(1)
+  return pdfTemplate({
+    template: './exampleFail.pdf',
+    output: './output.pdf',
+    data: {
+      name: 'John Doe',
+      age: 26,
+      email: 'johndoe@example.com',
+      birthdate: '01/01/1990',
+      projects: ['project1', 'project2', 'project3']
+    }
+  }).then((res) => {
+    t.false(res)
+  })
 })
